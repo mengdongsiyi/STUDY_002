db90d27aa7f2573d980eeebd03a598d6c32ac446
勉兴 吴 <wumianxing2019@outlook.com>
2023-03-08 11:09:04 +0800

    添加项目文件。

---
+++ STUDY_002/源.cpp
@@ -0,0 +1,65 @@
+#if 0
+
+/*调试代码*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdlib.h>
+#include<string>
+#include<stdio.h>
+
+int main()
+{
+	printf("hello mengdong...\n");
+	system("pause");
+	return 0;
+}
+
+
+
+#endif 
+
+#if 0
+/*调试类代码*/
+
+#include<iostream>
+#include<cstdlib>
+//using namespace std;
+class Parent
+{
+public:
+protected:
+private:
+};
+
+int main()
+{
+	std::cout << "hello mengdong...\n" << std::endl;
+	system("pause");
+	return 0;
+}
+
+
+#endif 
+
+
+#if 0
+#include<stdio.h>
+#include<stdlib.h>
+
+int add(int a, int b)
+{
+	printf("add do...\n");
+	return a + b;
+}
+
+int main()
+{
+	printf("我爱你一万年，爱的永恒！！！\n");
+	int a = 1000;
+	int b = 1001;
+	printf("a*b = %d\n", a * b);
+	printf("a+b = %d\n", add(a, b));
+	system("pause");
+	return 0;
+}
+#endif 
